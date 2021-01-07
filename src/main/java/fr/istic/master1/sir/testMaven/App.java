45e0f9802ce4cfe25a4dd72b5c1be56c6da2f062
Jean-Théodore ESSOH <essohjean88@gmail.com>
2021-01-07 22:18:52 +0100

    first commit

---
+++ src/main/java/fr/istic/master1/sir/testMaven/App.java
@@ -0,0 +1,15 @@
+package fr.istic.master1.sir.testMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	// Le main
+    public static void main( String[] args )
+    {
+    	// Affichage de "Hello world"
+        System.out.println( "Hello World!" );
+    }
+}
